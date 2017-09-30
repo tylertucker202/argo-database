8df9247001aeb88a8b40ec9040e4c7ec7043a6f3
tylertucker202 <tylertucker202@yahoo.com>
2017-09-29 17:05:34 -0700

    now can add by profile

---
+++ add_minor_dacs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 29 16:46:56 2017
+
+@author: gstudent4
+"""
+import logging
+import os
+from argoDatabase import argoDatabase
+
+if __name__ == '__main__':
+    FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(format=FORMAT,
+                        filename='add_minor_dacs.log',
+                        level=logging.DEBUG)
+    logging.debug('Start of log file')
+    HOME_DIR = os.getcwd()
+    OUTPUT_DIR = os.path.join('/home', 'gstudent4', 'Desktop', 'ifremer')
+    #OUTPUT_DIR = os.path.join('/home', 'gstudent4', 'Desktop', 'troublesome_files')
+    # init database
+    DB_NAME = 'argo_test'
+    COLLECTION_NAME = 'profiles'
+    DATA_DIR = os.path.join(HOME_DIR, 'data')
+
+    ad = argoDatabase(DB_NAME, COLLECTION_NAME)
+    minorDacs = ['nmdis', 'kordi', 'meds', 'kma', 'bodc', 'csio', 'incois' 'jma', 'csiro']
+    ad.add_locally(OUTPUT_DIR, how_to_add='by_dac_profiles', dacs=minorDacs)
--- rsyncAndListArgo.sh
+++ rsyncAndListArgo.sh
@@ -3,13 +3,16 @@
 echo 'Start of rsync and List'
 DATE=`date +%y-%m-%d-%H:%M`
 echo $DATE
-FTPDIR='/home/tyler/Desktop/argo/argo-database/ifremer/'
-#FTPDIR='/home/gstudent4/Desktop/ifremer/'
-QUEUEDIR='/home/tyler/Desktop/argo/argo-database/queuedFiles/'
-#QUEUEDIR='/home/gstudent4/Desktop/argo-database/queuedFiles/'
+#FTPDIR='/home/tyler/Desktop/argo/argo-database/ifremer/'
+FTPDIR='/home/gstudent4/Desktop/ifremer/'
+#QUEUEDIR='/home/tyler/Desktop/argo/argo-database/queuedFiles/'
+QUEUEDIR='/home/gstudent4/Desktop/argo-database/queuedFiles/'
 OUTPUTNAME=$QUEUEDIR'ALL-DACS-list-of-files-synced-'$DATE'.txt'
 echo 'Starting rsync: writing to '$FTPDIR
-rsync -arvzhim --delete --include='*/' --include='*_prof.nc' --exclude='*' vdmzrs.ifremer.fr::argo $FTPDIR > $OUTPUTNAME
+#Sync only *_prof.nc
+#rsync -arvzhim --delete --include='*/' --include='*_prof.nc' --exclude='*' vdmzrs.ifremer.fr::argo $FTPDIR > $OUTPUTNAME
+#Sync all .nc
+rsync -arvzhim --delete --include='*/' --include='*.nc' --exclude='*' vdmzrs.ifremer.fr::argo $FTPDIR > $OUTPUTNAME
 ENDDATE=`date +%y-%m-%d-%H:%M`
 echo 'End of rsync and List'
 echo $ENDDATE
