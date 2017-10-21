1b0832050cf63bf71b962c5edeb044c32b787682
tylertucker202 <tylertucker202@yahoo.com>
2017-10-20 18:20:43 -0700

    rsync now downloads specific files

--- rsyncAndListArgo.sh
+++ rsyncAndListArgo.sh
@@ -3,14 +3,18 @@
 echo 'Start of rsync and List'
 DATE=`date +%y-%m-%d-%H:%M`
 echo $DATE
-FTPDIR='/home/tyler/Desktop/argo/argo-database/ifremer/'
+FTPDIR='/storage/ifremer/'
 #FTPDIR='/home/gstudent4/Desktop/ifremer/'
 QUEUEDIR='/home/tyler/Desktop/argo/argo-database/queuedFiles/'
 #QUEUEDIR='/home/gstudent4/Desktop/argo-database/queuedFiles/'
 OUTPUTNAME=$QUEUEDIR'ALL-DACS-list-of-files-synced-'$DATE'.txt'
 echo 'Starting rsync: writing to '$FTPDIR
 #Sync only *_prof.nc
-rsync -arvzhim --delete --include='*/' --include='*_prof.nc' --exclude='*' vdmzrs.ifremer.fr::argo $FTPDIR > $OUTPUTNAME
+#rsync -arvzhim --delete --include='*/' --include='*_prof.nc' --exclude='*' vdmzrs.ifremer.fr::argo $FTPDIR > $OUTPUTNAME
+#Sync only /profiles
+#rsync -arvzhim --delete --include='**/' --include='**/profiles/**' --exclude='*' --exclude='**/profiles/B*' vdmzrs.ifremer.fr::argo/kordi $FTPDIR > $OUTPUTNAME
+#Sync only /profiles/[RDM]*
+rsync -arvzhim --delete --include='**/' --include='**/profiles/[RDM]*.nc' --exclude='*' --exclude='**/profiles/B*' vdmzrs.ifremer.fr::argo/coriolis $FTPDIR > $OUTPUTNAME
 #Sync all .nc
 #rsync -arvzhim --delete --include='*/' --include='*.nc' --exclude='*' vdmzrs.ifremer.fr::argo $FTPDIR > $OUTPUTNAME
 ENDDATE=`date +%y-%m-%d-%H:%M`
