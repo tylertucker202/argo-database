a2f6c1e4aeb14ddd7c3f78feab99e9b53a4cddde
tylertucker202 <tylertucker202@yahoo.com>
2018-08-08 16:03:14 -1000

    300-400

--- verdzi-api/space-time-grid.py
+++ verdzi-api/space-time-grid.py
@@ -57,8 +57,8 @@ if __name__ == '__main__':
     print('should be 37: {}'.format(len(datesSet[0:37])))
     print('should be 37: {}'.format(len(datesSet[-37:])))
 
-    continueAtIdx = 200
-    endAtIdx = 300
+    continueAtIdx = 300
+    endAtIdx = 400
     logging.debug('number of dates:{}'.format(len(datesSet)))
 
     for tdx, dates in enumerate(datesSet):
