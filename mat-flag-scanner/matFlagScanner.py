14de3dc8999d3feb7085afc3e3503268dd252531
tylertucker202 <tylertucker202@yahoo.com>
2018-08-09 12:13:20 -1000

    uses txt list

--- mat-flag-scanner/matFlagScanner.py
+++ mat-flag-scanner/matFlagScanner.py
@@ -7,23 +7,32 @@ Created on Sun Aug  5 11:44:42 2018
 """
 import os
 import glob
+import pdb
 import logging
 from numpy import ma as ma
 from scipy.io import savemat
 from open_argo_nc import open_Argo_ncfile
 
-def dummy_get_file_names(mirrorPath):
+def get_file_names(mirrorPath):
+    # alternative way to get list of profiles using regex
+    '''
     dacs = glob.glob(os.path.join(mirrorPath, '*'))
     file_names = []
     for dac in dacs:
         file_names = file_names+glob.glob(os.path.join(mirrorPath, dac, '**', 'profiles', '*.nc'))
-        
+    '''
+    # get list of profiles from text file.
     path_files = os.path.join(os.getcwd(), 'argovis-mat-python')
     path_flags = os.path.join(os.getcwd(), 'argovis-data')
     # filelist in input
     filename_list = os.path.join(path_flags, 'prof_fname_mirror.txt')
+    with open(filename_list) as f:
+        file_names = f.readlines()
+    file_names = [mirrorPath + os.sep + x.strip('\n') for x in file_names]
+    pdb.set_trace()
     path_argo_mirror = mirrorPath
     return file_names, path_files, path_argo_mirror, path_flags, filename_list
+
 
 if __name__ == '__main__':
     FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -38,13 +47,13 @@ if __name__ == '__main__':
         mirrorPath = os.path.join(localDir, os.pardir, 'troublesome-files')
     elif myhost == 'kadavu.ucsd.edu':
         logging.debug('in kadavu')
-        mirrorPath = os.path.join('home', 'tylertucker', 'ifremer')
+        mirrorPath = os.path.join(os.sep, 'home', 'tylertucker', 'ifremer')
     else:
         logging.debug('hostname unrecognized')
-        mirrorPath = os.path.join('home', 'tylertucker', 'ifremer')
+        mirrorPath = os.path.join(os.sep, 'home', 'tylertucker', 'ifremer')
 
     logging.debug('starting argoNCSandbox')
-    fileNames, filePaths, argoMirrorPath, flagPaths, filenameList = dummy_get_file_names(mirrorPath)
+    fileNames, filePaths, argoMirrorPath, flagPaths, filenameList = get_file_names(mirrorPath)
     flagVariables = ['flag_bad_pos_time','flag_bad_data','flag_bad_TEMP','flag_bad_PSAL','flag_no_file',]
     flags = {}
     for flagVar in flagVariables:
