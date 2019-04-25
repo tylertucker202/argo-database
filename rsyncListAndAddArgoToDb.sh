d3b72cffaa0544b1bce21edcc4dadeba4b5df7d0
tylertucker202 <tylertucker202@gmail.com>
2019-04-25 10:19:46 -1000

    adding test txt file

--- rsyncListAndAddArgoToDb.sh
+++ rsyncListAndAddArgoToDb.sh
@@ -18,6 +18,11 @@ case $HOSTNAME in
 		FTPDIR='/home/tylertucker/ifremer/'
 		ARGODIR='/home/tylertucker/Desktop/argo-database/add-profiles'
 		;;
+  (atoc02.colorado.edu)
+  		echo "this is kadavu"
+		FTPDIR='/data/argovis/ifremer/'
+		ARGODIR='/home/argovis/argo-database/add-profiles'
+		;;
 		(*) 
         	echo "this lab 416"
 		FTPDIR='/home/gstudent4/Desktop/ifremer/'
