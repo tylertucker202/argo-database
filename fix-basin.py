10d11b6dcf3130dd59c764fb25273b3fe3fed33e
tylertucker202 <tylertucker202@gmail.com>
2019-04-28 08:13:21 -1000

    logging on fix-basin

--- fix-basin.py
+++ fix-basin.py
@@ -2,29 +2,43 @@ import pymongo
 from netCDF4 import Dataset
 import numpy as np
 from scipy.interpolate import griddata
+import logging
 import pdb
+import os
+
+FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(format=FORMAT,
+                    filename='fix-basin.log',
+                    level=logging.DEBUG)  
 
 nc = Dataset('basinmask_01.nc', 'r')
 assert nc.variables['LONGITUDE'].mask == True
 assert nc.variables['LATITUDE'].mask == True
 
-idx = np.nonzero(~nc.variables['BASIN_TAG'][:].mask)
-basinVal = nc.variables['BASIN_TAG'][:][idx].astype('i')
-coords = np.stack([nc.variables['LATITUDE'][idx[0]],
-                   nc.variables['LONGITUDE'][idx[1]]]).T
-
+bdx = np.nonzero(~nc.variables['BASIN_TAG'][:].mask)
+basinVal = nc.variables['BASIN_TAG'][:][bdx].astype('i')
+coords = np.stack([nc.variables['LATITUDE'][bdx[0]],
+                   nc.variables['LONGITUDE'][bdx[1]]]).T
 
 dbUrl = 'mongodb://localhost:27017/'
 client = pymongo.MongoClient(dbUrl)
-db = client['argo2']
+db = client['argo-trouble']
 coll = db['profiles']
 
 cursor = coll.find()
+clen = coll.find().count()
+try:
+    for idx, doc in enumerate(cursor):
+        if idx % 2000 == 0:
+            logging.debug('on idx: {}'.format(idx))
+        try:
+            basin = int(griddata(coords, basinVal, (doc['lat'], doc['lon']), method='nearest'))
+        except Exception as err:
+            logging.warning('Exception occured: {0} for idx: {}'.format(idx, err))
+            basin = int(-999)
+            pass
+        coll.update({'_id': doc['_id']},{'$set': {'BASIN': basin} }, upsert=False, multi=False)
+    logging.debug('exited loop: idx {0} out of {1}'.format(idx, clen))
 
-for idx, doc in enumerate(cursor):
-    try:
-        basin = int(griddata(coords, basinVal, (doc['lat'], doc['lon']), method='nearest'))
-    except Exception:
-        basin = int(-999)
-        pass
-    coll.update({'_id': doc['_id']},{'$set': {'BASIN': basin} }, upsert=False, multi=False)
+finally:
+    logging.debug('exiting script: idx {0} out of {1}'.format(idx, clen))
